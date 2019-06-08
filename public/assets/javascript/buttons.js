90b45501eeaaebbc18b9fdded1ffee5c8bb57449
Natasha Giusti <natashagiusti@Tashs-Mac.local>
2019-06-08 13:42:36 -0700

    reformatted timestamp

--- public/assets/javascript/buttons.js
+++ public/assets/javascript/buttons.js
@@ -3,22 +3,37 @@ $(document).ready(function () {
     // Log buttons
     // Food
     $('#food').on('click', function () {
-        var foodTime = new Date();
-
+        var foodTime = new Date().toLocaleString(undefined, {
+            day: 'numeric',
+            month: 'numeric',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+        });
         console.log(foodTime);
     });
 
     // change
     $('#change').on('click', function () {
-        var changeTime = new Date();
-        
+        var changeTime = new Date().toLocaleString(undefined, {
+            day: 'numeric',
+            month: 'numeric',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+        });
         console.log(changeTime);
     });
 
     // Sleep
     $('#sleep').on('click', function () {
-        var sleepTime = new Date();
-
+        var sleepTime = new Date().toLocaleString(undefined, {
+            day: 'numeric',
+            month: 'numeric',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+        });
         console.log(sleepTime);
     })
 });
