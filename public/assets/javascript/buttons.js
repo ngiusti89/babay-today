bb498c30d3fe32e2528cac269e39f42e064ed27c
Natasha Giusti <natashagiusti@Tashs-Mac.local>
2019-06-08 11:22:30 -0700

    functioning buttons

--- public/assets/javascript/buttons.js
+++ public/assets/javascript/buttons.js
@@ -1,4 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    
-})
+    // Log buttons
+    // Food
+    $('#food').on('click', function () {
+        console.log("food clicked");
+    });
+
+    // Diaper
+    $('#diaper').on('click', function () {
+        console.log("diaper clicked");
+    });
+
+    // Sleep
+    $('#sleep').on('click', function () {
+        console.log("sleep clicked");
+    })
+});
