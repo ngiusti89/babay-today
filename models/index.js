5314b428bf0142a911021890e6cc1407ed8846b4
Ajay Yadav <ajay.yadav@AWKSOSX014.local>
2019-06-11 17:10:03 -0700

    added .env file and the logic

--- models/index.js
+++ models/index.js
@@ -5,7 +5,8 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+let config = require(__dirname + '/../config/config.js');
+config = config.dbConfig[env]
 const db = {};
 
 let sequelize;
