487fe030903cc62f65e49bfb5c976375ab01bdb2
Luke Graham <luke.evan.graham@gmail.com>
2019-06-11 21:29:30 -0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ exports.dbConfig = {
     password: process.env.DEVELOPMENT_PASSWORD,
     database: process.env.DEVELOPMENT_DB,
     host: process.env.DEVELOPMENT_HOST,
-    port: 8889,
+    port: 3306,
     dialect: "mysql"
 =======
     "username": "root",
