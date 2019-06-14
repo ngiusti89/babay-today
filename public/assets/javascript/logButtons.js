bf7fdca032bce39c5f9a7961a148dbbfafeb6e78
Natasha Giusti <natashagiusti@Tashs-Mac.local>
2019-06-13 19:07:41 -0700

    added quick log buttons

--- public/assets/javascript/logButtons.js
+++ public/assets/javascript/logButtons.js
@@ -17,10 +17,11 @@ $(document).ready(function () {
         var foodDiv = document.createElement('div');
 
         var quickLog = document.createElement('button');
-        var btnTxt = document.createTextNode("Quick log");
+        quickLog.innerHTML = "Quick Log";
         quickLog.setAttribute("class", "btn btn-lg")
-        quickLog.appendChild(btnTxt);
         foodOptions.appendChild(quickLog);
+        quickLog.appendChild(foodDiv);
+
 
         // TODO: post food timestamp
         // $.post("/api/food", foodTime, function (data) {
@@ -43,10 +44,10 @@ $(document).ready(function () {
         var changeDiv = document.createElement('div');
 
         var quickLog = document.createElement('button');
-        var btnTxt = document.createTextNode("Quick log");
+        quickLog.innerHTML = "Quick Log";
         quickLog.setAttribute("class", "btn btn-lg")
-        quickLog.appendChild(btnTxt);
         changeOptions.appendChild(quickLog);
+        quickLog.appendChild(changeDiv);
 
         // TODO: post change timestamp
         // $.post("/api/change", changeTime, function (data) {
@@ -58,6 +59,25 @@ $(document).ready(function () {
 
     // sleep
     $('#sleep').on('click', function () {
+
+
+        var sleepDiv = document.createElement('div');
+
+        var quickLog = document.createElement('button');
+        quickLog.innerHTML = "Quick Log";
+        quickLog.setAttribute("class", "btn btn-lg")
+        quickLog.setAttribute("id", "sleepQuickLog");
+        sleepOptions.appendChild(quickLog);
+        quickLog.appendChild(sleepDiv);
+
+        // TODO: post sleep timestamp
+        // $.post("/api/sleep", sleepTime, function (data) {
+        // $("")
+        // });
+
+    });
+
+    $('#sleepQuickLog').on('click', function () {
         var sleepTime = new Date().toLocaleString(undefined, {
             day: 'numeric',
             month: 'numeric',
@@ -66,19 +86,6 @@ $(document).ready(function () {
             minute: '2-digit',
         });
         console.log(sleepTime);
-
-        var sleepDiv = document.createElement('div');
-
-        var quickLog = document.createElement('button');
-        var btnTxt = document.createTextNode("Quick log");
-        quickLog.setAttribute("class", "btn btn-lg")
-        quickLog.appendChild(btnTxt);
-        sleepOptions.appendChild(quickLog);
-
-        // TODO: post sleep timestamp
-        // $.post("/api/sleep", sleepTime, function (data) {
-        // $("")
-        // });
-
     });
+
 });
